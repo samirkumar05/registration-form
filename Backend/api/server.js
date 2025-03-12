e389462a6783a5c1991e70bd4302369ff1e0517a
samirkumar05 <rpgpower48@gmail.com>
2025-03-12 22:16:17 +0530

    3  commit

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-
 const app = express();
-const port = process.env.PORT || 3000;
 
-// MongoDB Connection
-const DB = process.env.MONGODB_URI || 'mongodb+srv://dbUser:dbUserPassword@cluster0.dr27b.mongodb.net/expense';
+const port = process.env.PORT || 3000;
+const DB = 'mongodb+srv://dbUser:dbUserPassword@cluster0.dr27b.mongodb.net/expense';
 
 async function connectToDatabase() {
     try {
@@ -16,21 +14,22 @@ async function connectToDatabase() {
         console.error("Connection failed", err);
     }
 }
+
 connectToDatabase();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve Static Files
+// Serve static frontend files
 app.use(express.static(path.join(__dirname, "../Frontend")));
 
+// Creating our Home Page Route
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend", "index.html"));
 });
 
 const User = require("./model/registration.model.js");
 
-// API Route for Registration
 app.post("/api/register", async (req, res) => {
     const { name, email, password } = req.body;
     const newUser = new User({ name, email, password });
